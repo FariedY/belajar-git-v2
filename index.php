6eb0269a3189695f472e7fa8f07b1bdc3b986fcf
FariedY <farriedy@gmail.com>
2023-03-24 00:07:20 +0700

    mnmbahkn paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, voluptas illum accusantium voluptate sit mollitia quos ipsam officia a velit praesentium ducimus alias earum qui quasi dolore maiores, ipsum explicabo?</p>
 </body>
 </html>
